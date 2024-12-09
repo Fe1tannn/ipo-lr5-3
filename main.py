b41a1b75f4b21c7b3ad1d32fb0526ecb3cf0e947
Yan <tal531764@gmail.com>
2024-12-10 01:16:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-with open ("text.txt", "r", encoding="utf-8") as file:
-    a = file.read()
-    b = a.split()
-print(len(b))
+with open ("text.txt", "r", encoding="utf-8") as file: #Открываем файл
+    a = file.read() #Сохраняем данные файла в переменной a
+    b = a.split() #Сохраняем разделенные слова в список b
+print(len(b)) #Выводим количество слов
